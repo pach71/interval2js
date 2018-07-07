5a8ce5c22ca513d21184361f811d786598598a3c
Payam Chamani <pach71@users.noreply.github.com>
2018-07-07 16:35:17 +0430

    sfd

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports = function(func, interval){
-    if (typeof func === 'function') {
+    if (typeof func === 'function')
         func();
-        setInterval(func, interval);
-    }else{
-        setInterval(func, interval);
-    }
+    return setInterval(func, interval);
 }
